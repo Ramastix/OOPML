fa155f5fd6457c98ba023759a11d379024841522
Ramastix <ramastix@gmail.com>
2023-02-13 12:04:03 +0300

    initial2

--- src/main/java/Conditioner.java
+++ src/main/java/Conditioner.java
@@ -1,4 +1,25 @@
 public class Conditioner {
     public int currentTemperature;
 
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int newCurrentTemperature) {
+        if (newCurrentTemperature < 0) {
+            return;
+        }
+        if (newCurrentTemperature > 30) {
+            return;
+        }
+        currentTemperature = newCurrentTemperature;
+    }
+
+    public void increaseTemp50p() {
+        int target = currentTemperature *3 / 2;
+        setCurrentTemperature(target);
+    }
+    public void setToMaxTemp() {
+        currentTemperature = 30;
+    }
 }
--- src/test/java/ConditionerTest.java
+++ src/test/java/ConditionerTest.java
@@ -1,6 +1,54 @@
-public class ConditionerTest {
-    public class ConditionerTest {
-        @Test
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class ConditionerTest {
+
+    @Test
+    public void shouleSetTemperature() {
+        Conditioner cond = new Conditioner();
+
+        cond.setCurrentTemperature(15);
+
+        int expected = 15;
+        int actual = cond.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetTemperatureAboveMax() {
+        Conditioner cond = new Conditioner();
+
+        cond.setCurrentTemperature(50);
+
+        int expected = 0;
+        int actual = cond.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void  shouldSetToMaxTemperature() {
+        Conditioner cond = new Conditioner();
+
+        cond.setToMaxTemp();
+
+        int expected = 30;
+        int actual = cond.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseTemperature50p() {
+        Conditioner cond = new Conditioner();
+        cond.setCurrentTemperature(25);
+
+        cond.increaseTemp50p();
+
+        int expected = 25;
+        int actual = cond.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
     }
 }
